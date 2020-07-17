0330f1cb9747b92ee7d71e8524e8bcd3828fd923
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-17 23:23:20 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -13,5 +13,12 @@ class Project
   
   def add_backer(backer)
     @backers << backer
+    backer.backed_projects << self
   end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+  
 end
